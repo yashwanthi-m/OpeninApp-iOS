f67444d4147bc6e406658d731344df6bc33cbdd3
yashwanthi-m <manchalayashwanthiofficial@gmail.com>
2023-06-19 13:12:05 +0530

    Initial Commit

---
+++ OpeninApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OpeninApp
+//
+//  Created by Yashwanthi Manchala on 19/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OpeninApp/OpeninAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeninAppApp.swift
+//  OpeninApp
+//
+//  Created by Yashwanthi Manchala on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpeninAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
